7faa180bf7b1df538baf45d26c5114b0615331b8
Anže Marinko <anze.marinko96@gmail.com>
2025-02-02 21:37:19 +0100

    improve icons and add refresh and footer

--- social_lite/app.js
+++ social_lite/app.js
@@ -157,3 +157,8 @@ function displayVideos(channel, videos) {
 if (getApiKey()) {
     fetchVideos();
 }
+
+function toggleTooltip(id) {
+    const tooltip = document.getElementById(id);
+    tooltip.style.display = tooltip.style.display === 'block' ? 'none' : 'block';
+}
